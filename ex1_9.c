c0031e5ab459a855344247cb0eca196fe3898750
Danny Han <cool.huip@gmail.com>
2023-05-02 18:40:05 -0400

    Create ex1_9.c

---
+++ ex1_9.c
@@ -0,0 +1,25 @@
+/*
+ * "The C Programming Language" - Brian W. Kernighan, Dennis M. Ritchie
+ * Second Edition
+ *
+ * Exercise 1-9: Write a program to copy its input to its output, replacing
+ *               each string of one or more blanks by a single blank.
+ *
+ */
+#include <stdio.h>
+
+
+/*
+ * Basically, if there are more than one whitespace between any two words,
+ * replace those whitespaces with a single one.
+ */
+int main() {
+}
+
+/* OUTPUT
+ *
+ * >>> cc ex1_9.c
+ * >>> ./a.out
+ *
+ * >>>
+ */
