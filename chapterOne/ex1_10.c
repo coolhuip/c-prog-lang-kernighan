110d104f6ddb65e9250db9c117f38f9084a7d51e
Danny Han <cool.huip@gmail.com>
2023-05-03 04:39:00 -0400

    Create ex1_10.c

---
+++ chapterOne/ex1_10.c
@@ -0,0 +1,19 @@
+/*
+ * "The C Programming Language" - Brian W. Kernighan, Dennis M. Ritchie
+ * Second Edition
+ *
+ * Exercise 1-10: 
+ *
+ */
+
+
+int main() {
+}
+
+/* OUTPUT
+ *
+ * >>> cc ex1_10.c
+ * >>> ./a.out
+ *
+ * >>>
+ */
