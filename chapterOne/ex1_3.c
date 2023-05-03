7c5bb66c0168f2a8b50c87a09806f22c8c75e86a
Danny Han <cool.huip@gmail.com>
2023-05-03 05:01:49 -0400

    Remove unnecessary code

--- chapterOne/ex1_3.c
+++ chapterOne/ex1_3.c
@@ -8,45 +8,7 @@
 #include <stdio.h>
 
 
-/*
- * Formula: C = (5/9)(F-32)
- */
-void intFahrCelciusConversion() {
-	int fahr, celcius;
-	int lower, upper, step;
-
-	lower = 0;
-	upper = 300;
-	step = 20;
-
-	fahr = lower;
-	while (fahr <= upper) {
-		// celcius = (5/9) * (fahr - 32);  <-- You CANNOT do it this way...
-		// 				       ...b/c: The operation 5/9 yields a truncated integer ZERO.
-		//                                             You would, instead, need 5.0/9.0 if celcius is FLOAT.
-		celcius = 5 * (fahr - 32) / 9;
-		printf("%3d%6d\n", fahr, celcius);
-		fahr += step;
-	}
-}
-
-void floatFahrCelciusConversion() {
-	float fahr, celcius;
-	int lower, upper, step;
-
-	lower = 0;
-	upper = 300;
-	step = 20;
-
-	fahr = lower;
-	while (fahr <= upper) {
-		celcius = (5.0/9.0) * (fahr - 32);
-		printf("%5.1f%6.1f\n", fahr, celcius);
-		fahr += step;
-	}
-}
-
-void modifiedFahrCelciusConversion() {
+int main() {
 	float fahr, celcius;
 	int lower, upper, step;
 	char strFahr[] = "FAHRENHEIT";
@@ -63,10 +25,6 @@ void modifiedFahrCelciusConversion() {
 		printf("%10.1f%10.1f\n", fahr, celcius);
 		fahr += step;
 	}
-}
-
-int main() {
-	modifiedFahrCelciusConversion();
 }
 
 /* OUTPUT
