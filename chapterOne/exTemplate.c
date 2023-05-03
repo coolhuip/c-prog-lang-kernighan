4ac9af2c6d5ff7adf9e2d418363da0aa35ba2273
Danny Han <cool.huip@gmail.com>
2023-05-03 04:10:03 -0400

    Move to new directory

---
+++ chapterOne/exTemplate.c
@@ -0,0 +1,19 @@
+/*
+ * "The C Programming Language" - Brian W. Kernighan, Dennis M. Ritchie
+ * Second Edition
+ *
+ * Exercise #-#:
+ *
+ */
+
+
+int main() {
+}
+
+/* OUTPUT
+ *
+ * >>> cc ex#_#.c
+ * >>> ./a.out
+ *
+ * >>>
+ */
