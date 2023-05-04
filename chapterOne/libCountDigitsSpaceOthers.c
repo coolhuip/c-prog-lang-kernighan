6b874d0e21ba06cd204af4bd89f4b34ea894db6b
Danny Han <cool.huip@gmail.com>
2023-05-04 00:08:27 -0400

    Minor changes

--- chapterOne/libCountDigitsSpaceOthers.c
+++ chapterOne/libCountDigitsSpaceOthers.c
@@ -12,6 +12,20 @@
  * Count the number of occurrences of each digit, of white space characters
  * (blank, tab, newline), and of all other characters.
  */
-int main() {
-}
+void countDigitsSpaceOthers() {
+	int c, i, nWhite, nOther;
+	int nDigit[10];
 
+	nWhite = nOther = 0;
+	for (i = 0; i < 10; i++) nDigit[i] = 0; // Initialize array of zeros.
+
+	while ((c = getchar()) != EOF) {
+		if (c >= '0' && c <= '9') nDigit[c - '0']++;
+		else if (c == '\n' || c == '\t' || c == ' ') nWhite++;
+		else nOther++;
+	}
+
+	printf("Digits:");
+	for (i = 0; i < 10; i++) printf(" %d", nDigit[i]);
+	printf("\nWhite spaces: %d\nOthers: %d\n", nWhite, nOther);
+}
