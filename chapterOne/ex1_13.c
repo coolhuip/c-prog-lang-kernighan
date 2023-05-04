a69279db50af35a29310085bc6fb1efd2d76d80f
Danny Han <cool.huip@gmail.com>
2023-05-03 22:30:37 -0400

    Create ex1_13.c

---
+++ chapterOne/ex1_13.c
@@ -0,0 +1,20 @@
+/*
+ * "The C Programming Language" - Brian W. Kernighan, Dennis M. Ritchie
+ * Second Edition
+ *
+ * Exercise 1-13: Write a program to print a histogram of the lengths of words
+ *                in its input. It's easy to draw the histogram with the bars
+ *                horizontal; a vertical orientation is more challenging.
+ */
+
+
+int main() {
+}
+
+/* OUTPUT
+ *
+ * >>> cc ex1_13.c
+ * >>> ./a.out
+ *
+ * >>>
+ */
